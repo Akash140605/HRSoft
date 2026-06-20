b416168f341c3859be6b4ddbc57f4f17d18822db
AkashGupta <akashcomputerbisalpur@gmail.com>
2026-06-20 13:44:20 +0530

    hi

--- src/data/defaultData.js
+++ src/data/defaultData.js
@@ -1,9 +1,9 @@
 export const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export const SHIFT_OPTIONS = [
-  { code: 'A', label: 'Shift A', start: '06:00', end: '14:00', hours: 8 },
-  { code: 'B', label: 'Shift B', start: '14:00', end: '22:00', hours: 8 },
-  { code: 'C', label: 'Shift C', start: '22:00', end: '06:00', hours: 8 },
+  { code: 'A', label: 'Shift A', start: '05:00', end: '14:00', hours: 8 },
+  { code: 'B', label: 'Shift B', start: '13:00', end: '22:00', hours: 8 },
+  { code: 'C', label: 'Shift C', start: '21:00', end: '06:00', hours: 8 },
   { code: 'AA', label: 'Shift AA', start: '08:00', end: '20:00', hours: 12 },
   { code: 'BB', label: 'Shift BB', start: '20:00', end: '08:00', hours: 12 }
 ];
