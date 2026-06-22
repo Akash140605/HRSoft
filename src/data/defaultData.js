b4ec7499d6476bfe956e1d5fd3b06583b718cdb6
AkashGupta <akashcomputerbisalpur@gmail.com>
2026-06-22 10:23:24 +0530

    hi

--- src/data/defaultData.js
+++ src/data/defaultData.js
@@ -4,8 +4,8 @@ export const SHIFT_OPTIONS = [
   { code: 'A', label: 'Shift A', start: '05:00', end: '14:00', hours: 8 },
   { code: 'B', label: 'Shift B', start: '13:00', end: '22:00', hours: 8 },
   { code: 'C', label: 'Shift C', start: '21:00', end: '06:00', hours: 8 },
-  { code: 'AA', label: 'Shift AA', start: '08:00', end: '19:00', hours: 12 },
-  { code: 'BB', label: 'Shift BB', start: '20:00', end: '07:00', hours: 12 }
+  { code: 'AA', label: 'Shift AA', start: '07:00', end: '20:00', hours: 12 },
+  { code: 'BB', label: 'Shift BB', start: '19:00', end: '08:00', hours: 12 }
 ];
 
 export const DEFAULT_HALLS = [
